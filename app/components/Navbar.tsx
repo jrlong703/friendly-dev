cb35e5ef89630a2c856c8e4f1a05b5d2774d1dd8
J.R. Long <jrlong@outlook.com>
2025-10-06 14:36:42 -0400

    [11:103] Response menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import { NavLink } from 'react-router';
-import { FaLaptopCode } from 'react-icons/fa';
+import { FaBars, FaLaptopCode, FaTimes } from 'react-icons/fa';
 
 const Navbar: FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const base = 'transition hover:text-blue-400';
   const active = 'text-blue-400 font-semibold';
 
@@ -52,7 +54,59 @@ const Navbar: FC = () => {
             </NavLink>
           </div>
         </div>
+
+        <div className='flex items-center gap-4 md:hidden'>
+          <button
+            type='button'
+            title='Menu'
+            onClick={() => setMenuOpen(!menuOpen)}
+            className='cursor-pointer text-xl text-blue-400'
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </div>
+
+      {/* Mobile Nav */}
+      {menuOpen && (
+        <div className='space-y-2 space-x-4 border-t border-gray-700 bg-gray-800 px-6 py-4 text-center md:hidden'>
+          <NavLink
+            to='/'
+            onClick={() => setMenuOpen(false)}
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to='/projects'
+            onClick={() => setMenuOpen(false)}
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to='/blog'
+            onClick={() => setMenuOpen(false)}
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            to='/about'
+            onClick={() => setMenuOpen(false)}
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to='/contact'
+            onClick={() => setMenuOpen(false)}
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Contact
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 };
