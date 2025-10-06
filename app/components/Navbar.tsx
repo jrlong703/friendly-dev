29f8a77367d76009ccd456603380ed598fecca30
J.R. Long <jrlong@outlook.com>
2025-10-06 14:17:15 -0400

    [11:102] Active link styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,6 +3,9 @@ import { NavLink } from 'react-router';
 import { FaLaptopCode } from 'react-icons/fa';
 
 const Navbar: FC = () => {
+  const base = 'transition hover:text-blue-400';
+  const active = 'text-blue-400 font-semibold';
+
   return (
     <nav className='a-50 sticky top-0 border-b border-gray-700 bg-gray-800 shadow-md'>
       <div className='mx-auto flex max-w-6xl items-center justify-between px-6 py-4'>
@@ -17,11 +20,36 @@ const Navbar: FC = () => {
         {/* Desktop Nav */}
         <div className='hidden items-center gap-6 md:flex'>
           <div className='space-x-4 text-sm text-gray-300'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/projects'>Projects</NavLink>
-            <NavLink to='/blog'>Blog</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/contact'>Contact</NavLink>
+            <NavLink
+              to='/'
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to='/projects'
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to='/blog'
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              to='/about'
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to='/contact'
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Contact
+            </NavLink>
           </div>
         </div>
       </div>
