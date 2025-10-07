09af8d338de729703f037a6658ca516ec20c448d
J.R. Long <jrlong@outlook.com>
2025-10-07 13:50:40 -0400

    [13:124] Fetch post meta data

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,24 @@
 import type { FC } from 'react';
 
-const BlogPage: FC = () => {
+import type { Route } from './+types/index.tsx';
+
+export const loader = async ({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> => {
+  const url = new URL('/data/posts-meta.json', request.url);
+  const response = await fetch(url.href);
+  if (!response.ok) throw new Error('Failed to fetch data');
+
+  const data = await response.json();
+
+  return { posts: data };
+};
+
+const BlogPage: FC<Route.ComponentProps> = ({ loaderData }) => {
+  const { posts } = loaderData;
+
+  console.log(posts);
+
   return (
     <>
       <h2 className='mb-8 text-3xl font-bold text-white'>📝Blog</h2>
