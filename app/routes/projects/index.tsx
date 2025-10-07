a0a12152387fae7aa1d561f27a8686945bccad2a
J.R. Long <jrlong@outlook.com>
2025-10-07 06:54:37 -0400

    [12:108] Fetch projects with loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,18 @@
 import type { FC } from 'react';
 
-const ProjectsPage: FC = () => {
+import type { Route } from './+types/index';
+
+export const loader = async ({ request }: Route.LoaderArgs): Promise<any> => {
+  const response = await fetch('http://localhost:5000/projects');
+  const data = await response.json();
+
+  return { projects: data };
+};
+
+const ProjectsPage: FC<Route.ComponentProps> = ({ loaderData }) => {
+  const { projects } = loaderData;
+  console.log(projects);
+
   return (
     <>
       <h2 className='mb-8 text-3xl font-bold text-white'>🚀Projects</h2>
